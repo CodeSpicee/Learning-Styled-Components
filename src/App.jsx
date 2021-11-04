faa39719e12bbad5436b5aad37f96461a207fa68
unknown <afolabibidemi63@yahoo.com>
2021-11-04 11:39:51 +0100

    Styled-Component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./components/styles/Container.styled"
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+}
+
+export default App
